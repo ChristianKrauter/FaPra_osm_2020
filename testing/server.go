8d809df4e4ced3e90e2b889ae6062b457dae9df2
Chris <chris.a.krauter@gmail.com>
2020-06-06 22:16:18 +0200

    more dijkstra (broken)

--- testing/server.go
+++ testing/server.go
@@ -15,7 +15,11 @@ import (
 )
 
 var port int = 8081
-var meshgrid [][]bool
+var meshgrid []bool
+var meshgrid2d [][]bool
+var dist []float64
+var prev []int64
+var vertices []int64
 
 func check(e error) {
 	if e != nil {
@@ -34,10 +38,10 @@ func toGeojson(route [][]float64) []byte {
 func neigbourghs(point []int64) [][]int64 {
 	var neigbourghs [][]int64
 
-	var xPlus1  = int64(math.Mod(float64(point[0] + 1),360.0))
-	var xMinus1 = int64(math.Mod(float64(point[0] - 1),360.0))
-	var yPlus1  = int64(math.Mod(float64(point[1] + 1),360.0))
-	var yMinus1 = int64(math.Mod(float64(point[1] - 1),360.0))
+	var xPlus1 = int64(math.Mod(float64(point[0]+1), 360.0))
+	var xMinus1 = int64(math.Mod(float64(point[0]-1), 360.0))
+	var yPlus1 = int64(math.Mod(float64(point[1]+1), 360.0))
+	var yMinus1 = int64(math.Mod(float64(point[1]-1), 360.0))
 
 	neigbourghs = append(neigbourghs, []int64{xPlus1, point[1]})
 	neigbourghs = append(neigbourghs, []int64{xPlus1, yPlus1})
@@ -50,21 +54,59 @@ func neigbourghs(point []int64) [][]int64 {
 	return neigbourghs
 }
 
+func neigbourghs1d(point int64) []int64 {
+	var neigbourghs [][]int64
+
+	var xPlus1 = int64(math.Mod(float64(point[0]+1), 360.0))
+	var xMinus1 = int64(math.Mod(float64(point[0]-1), 360.0))
+	var yPlus1 = int64(math.Mod(float64(point[1]+1), 360.0))
+	var yMinus1 = int64(math.Mod(float64(point[1]-1), 360.0))
+
+	neigbourghs = append(neigbourghs, point+1)
+	neigbourghs = append(neigbourghs, point-1)
+	neigbourghs = append(neigbourghs, point-len(meshgrid2d))
+	neigbourghs = append(neigbourghs, []int64{xMinus1, point[1]})
+	neigbourghs = append(neigbourghs, []int64{xMinus1, yPlus1})
+	neigbourghs = append(neigbourghs, []int64{xMinus1, yMinus1})
+	neigbourghs = append(neigbourghs, []int64{point[0], yPlus1})
+	neigbourghs = append(neigbourghs, []int64{point[0], yMinus1})
+	return neigbourghs
+}
+
 func gridToCoord(in []int64) []float64 {
 	var out []float64
-	out = append(out, float64(in[0] - 180))
-	out = append(out, float64((in[1] / 2) - 90))
+	out = append(out, float64(in[0]-180))
+	out = append(out, float64((in[1]/2)-90))
 	return out
 }
 
 func coordToGrid(in []float64) []int64 {
 	var out []int64
-	out = append(out, int64(math.Round(in[0])) + 180)
-	out = append(out, (int64(math.Round(in[1])) + 90) * 2)
+	out = append(out, int64(math.Round(in[0]))+180)
+	out = append(out, (int64(math.Round(in[1]))+90)*2)
 	return out
 }
 
-func dijkstra(startLng float64, startLat float64, endLng float64, endLat float64) [][]float64 {
+func remove(s []int64, i int64) []int64 {
+    s[i] = s[len(s)-1]
+    return s[:len(s)-1]
+}
+
+func min(dist *[]float64, vert *[]int64) []int64{
+	var min = math.Inf(-1)
+	var argmin int64
+	var arschmin int64
+	for j, i := range *vert {
+		if (*dist)[i] < min {
+			min = (*dist)[i]
+			argmin = i
+			arschmin = int64(j)
+		}
+	}
+	return []int64{argmin, arschmin}
+}
+
+func dijkstra(startLng float64, startLat float64, endLng float64, endLat float64, startLngInt int64, startLatInt int64, endLngInt int64, endLatInt int64) [][]float64 {
 
 	var route [][]float64
 	route = append(route, []float64{math.Round(startLng), math.Round(startLat)})
@@ -77,8 +119,33 @@ func dijkstra(startLng float64, startLat float64, endLng float64, endLat float64
 	//	route = append(route, gridToCoord(k))
 	//}
 
-	fmt.Printf("%v/", meshgrid[int64(math.Round(startLng))+180][(int64(math.Round(startLat))+90)*2])
-	fmt.Printf("%v\n", meshgrid[int64(math.Round(endLng))+180][(int64(math.Round(endLat))+90)*2])
+	fmt.Printf("%v/", meshgrid2d[int64(math.Round(startLng))+180][(int64(math.Round(startLat))+90)*2])
+	fmt.Printf("%v\n", meshgrid2d[int64(math.Round(endLng))+180][(int64(math.Round(endLat))+90)*2])
+
+	for i := range meshgrid {
+		dist = append(dist, math.Inf(-1))
+		prev = append(prev, -1)
+		vertices = append(vertices, int64(i))
+	}
+	print("hello\n")
+	print(len(dist))
+	print("hello\n")
+	// this is wrong i think, we need the length of meshgrid2d[0] glaub
+	dist[(int64(len(meshgrid2d))*startLngInt)+startLatInt] = 0
+
+	for {
+		if len(vertices) == 0 {
+			break
+		} else {
+			var whatever = min(&dist, &vertices)
+			var u = whatever[0]
+			print(u)
+			remove(vertices, whatever[1])
+
+			for i := neigbourghs(u)
+		}
+
+	}
 
 	return route
 }
@@ -91,7 +158,12 @@ func main() {
 	}
 	defer meshgridRaw.Close()
 	byteValue, _ := ioutil.ReadAll(meshgridRaw)
-	json.Unmarshal(byteValue, &meshgrid)
+	json.Unmarshal(byteValue, &meshgrid2d)
+	for i := 0; i < len(meshgrid2d); i++ {
+		meshgrid = append(meshgrid, meshgrid2d[i]...)
+	}
+	//fmt.Printf("%v",meshgrid)
+	//fmt.Printf("%v",len(meshgrid))
 	//fmt.Printf("%v", meshgrid)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -115,13 +187,28 @@ func main() {
 				panic(err3)
 			}
 
-			fmt.Printf("start: %d / %d ", int64(math.Round(startLat)), int64(math.Round(startLng)))
-			fmt.Printf("end: %d / %d\n", int64(math.Round(endLat)), int64(math.Round(endLng)))
+			var start = coordToGrid([]float64{startLng, startLat})
+			var startLngInt = start[0]
+			var startLatInt = start[1]
 
-			var route = dijkstra(startLng, startLat, endLng, endLat)
-			var result = toGeojson(route)
+			var end = coordToGrid([]float64{endLng, endLat})
+			var endLngInt = end[0]
+			var endLatInt = end[1]
 
-			w.Write(result)
+			fmt.Printf("\n%v/%v, %v/%v\n", startLngInt, startLatInt, endLngInt, endLatInt)
+
+			if !meshgrid2d[startLatInt][startLngInt] && !meshgrid2d[endLatInt][endLngInt] {
+				fmt.Printf("start: %d / %d ", int64(math.Round(startLat)), int64(math.Round(startLng)))
+				fmt.Printf("end: %d / %d\n", int64(math.Round(endLat)), int64(math.Round(endLng)))
+
+				var route = dijkstra(startLng, startLat, endLng, endLat, startLngInt, startLatInt, endLngInt, endLatInt)
+				var result = toGeojson(route)
+
+				w.Write(result)
+			} else {
+				w.Write([]byte("false"))
+			}
+
 		} else {
 			http.ServeFile(w, r, r.URL.Path[1:])
 		}
