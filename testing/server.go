5df7cd327ca091fc7147a3858977afac9c7d7ded
Chris <chris.a.krauter@gmail.com>
2020-06-07 17:57:16 +0200

    Dijkstra progress, neigbourghs not wrapping

--- testing/server.go
+++ testing/server.go
@@ -17,6 +17,7 @@ import (
 var port int = 8081
 var meshgrid []bool
 var meshgrid2d [][]bool
+var meshWidth int64
 var dist []float64
 var prev []int64
 var vertices []int64
@@ -55,21 +56,16 @@ func neigbourghs(point []int64) [][]int64 {
 }
 
 func neigbourghs1d(point int64) []int64 {
-	var neigbourghs [][]int64
+	var neigbourghs []int64
 
-	var xPlus1 = int64(math.Mod(float64(point[0]+1), 360.0))
-	var xMinus1 = int64(math.Mod(float64(point[0]-1), 360.0))
-	var yPlus1 = int64(math.Mod(float64(point[1]+1), 360.0))
-	var yMinus1 = int64(math.Mod(float64(point[1]-1), 360.0))
-
-	neigbourghs = append(neigbourghs, point+1)
-	neigbourghs = append(neigbourghs, point-1)
-	neigbourghs = append(neigbourghs, point-len(meshgrid2d))
-	neigbourghs = append(neigbourghs, []int64{xMinus1, point[1]})
-	neigbourghs = append(neigbourghs, []int64{xMinus1, yPlus1})
-	neigbourghs = append(neigbourghs, []int64{xMinus1, yMinus1})
-	neigbourghs = append(neigbourghs, []int64{point[0], yPlus1})
-	neigbourghs = append(neigbourghs, []int64{point[0], yMinus1})
+	neigbourghs = append(neigbourghs, point-meshWidth-1) // top left
+	neigbourghs = append(neigbourghs, point-meshWidth)   // top
+	neigbourghs = append(neigbourghs, point-meshWidth+1) // top right
+	neigbourghs = append(neigbourghs, point-1)           // left
+	neigbourghs = append(neigbourghs, point+1)           // right
+	neigbourghs = append(neigbourghs, point+meshWidth-1) // bottom left
+	neigbourghs = append(neigbourghs, point+meshWidth)   // bottom
+	neigbourghs = append(neigbourghs, point+meshWidth+1) // bottom right
 	return neigbourghs
 }
 
@@ -87,27 +83,71 @@ func coordToGrid(in []float64) []int64 {
 	return out
 }
 
-func remove(s []int64, i int64) []int64 {
-    s[i] = s[len(s)-1]
-    return s[:len(s)-1]
+func flattenIndx(lat int64, lng int64) int64 {
+	return ((int64(meshWidth) * lat) + lng)
 }
 
-func min(dist *[]float64, vert *[]int64) []int64{
-	var min = math.Inf(-1)
+func expandIndx(indx int64) []float64 {
+	var lat = float64(indx / meshWidth)
+	var lng = float64(indx % meshWidth)
+	return []float64{lat, lng}
+}
+
+func remove(s []int64, i int64) []int64 {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func min(dist *[]float64, vertices *[]int64) []int64 {
+	var min = math.Inf(1)
 	var argmin int64
-	var arschmin int64
-	for j, i := range *vert {
+	var argminIndx int64
+
+	for j, i := range *vertices {
 		if (*dist)[i] < min {
 			min = (*dist)[i]
 			argmin = i
-			arschmin = int64(j)
+			argminIndx = int64(j)
 		}
 	}
-	return []int64{argmin, arschmin}
+	return []int64{argmin, argminIndx}
 }
 
-func dijkstra(startLng float64, startLat float64, endLng float64, endLat float64, startLngInt int64, startLatInt int64, endLngInt int64, endLatInt int64) [][]float64 {
+func haversin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
 
+func distance(start, end []float64) float64 {
+	var fLat, fLng, fLat2, fLng2, radius float64
+	fLat  = start[0] * math.Pi / 100
+	fLng  = start[1] * math.Pi / 100
+	fLat2 = end[0] * math.Pi / 100
+	fLng2 = end[1] * math.Pi / 100
+
+	radius = 6378100
+	h := haversin(fLat2-fLat) + math.Cos(fLat)*math.Cos(fLat2)*haversin(fLng2-fLng)
+
+	return (2 * radius * math.Asin(math.Sqrt(h)))
+}
+
+func extractRoute(prev []int64, end int64) [][]float64{
+	print("started expand route\n")
+	var route [][]float64
+
+	for{
+		route = append(route, expandIndx(end))
+		if prev[end] == -1 {
+			break
+		}
+		end = prev[end]
+	}
+	fmt.Printf("%v\n", route)
+	return route
+}
+
+func dijkstra(startLng, startLat, endLng, endLat float64, startLngInt, startLatInt, endLngInt, endLatInt int64) [][]float64 {
+
+	print("started Dijkstra\n")
 	var route [][]float64
 	route = append(route, []float64{math.Round(startLng), math.Round(startLat)})
 	route = append(route, []float64{math.Round(endLng), math.Round(endLat)})
@@ -123,31 +163,44 @@ func dijkstra(startLng float64, startLat float64, endLng float64, endLat float64
 	fmt.Printf("%v\n", meshgrid2d[int64(math.Round(endLng))+180][(int64(math.Round(endLat))+90)*2])
 
 	for i := range meshgrid {
-		dist = append(dist, math.Inf(-1))
+		dist = append(dist, math.Inf(1))
 		prev = append(prev, -1)
 		vertices = append(vertices, int64(i))
 	}
-	print("hello\n")
-	print(len(dist))
-	print("hello\n")
-	// this is wrong i think, we need the length of meshgrid2d[0] glaub
-	dist[(int64(len(meshgrid2d))*startLngInt)+startLatInt] = 0
 
+	//// Flatten / expand test
+	//var tempFlattened int64
+	//tempFlattened = flattenIndx(startLatInt, startLngInt)
+	//fmt.Printf("lat/lng: %v / %v\n", startLatInt, startLngInt)
+	//fmt.Printf("flatten: %v\n", tempFlattened)
+	//fmt.Printf("expand : %v\n", expandIndx(tempFlattened))
+	//os.Exit(-1)
+
+	dist[flattenIndx(startLatInt, startLngInt)] = 0
+	print("starting loop\n")
 	for {
 		if len(vertices) == 0 {
 			break
 		} else {
 			var whatever = min(&dist, &vertices)
 			var u = whatever[0]
-			print(u)
+			//fmt.Printf("u: %v\n", u)
 			remove(vertices, whatever[1])
 
-			for i := neigbourghs(u)
+			for _, j := range neigbourghs1d(u) {
+				//fmt.Printf("j: %v, land:%v\n", j, meshgrid[j])
+				if !meshgrid[j] {
+					var alt = dist[u] + distance(expandIndx(u), expandIndx(j))
+					if alt < dist[j] {
+						dist[j] = alt
+						prev[j] = u
+					}
+				}
+			}
 		}
-
 	}
 
-	return route
+	return extractRoute(prev, flattenIndx(endLatInt, endLngInt))
 }
 
 func main() {
@@ -159,12 +212,11 @@ func main() {
 	defer meshgridRaw.Close()
 	byteValue, _ := ioutil.ReadAll(meshgridRaw)
 	json.Unmarshal(byteValue, &meshgrid2d)
+
+	meshWidth = int64(len(meshgrid2d[0]))
 	for i := 0; i < len(meshgrid2d); i++ {
 		meshgrid = append(meshgrid, meshgrid2d[i]...)
 	}
-	//fmt.Printf("%v",meshgrid)
-	//fmt.Printf("%v",len(meshgrid))
-	//fmt.Printf("%v", meshgrid)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
