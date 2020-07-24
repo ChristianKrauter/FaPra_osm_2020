2c77baac9adb9f84b0e2f0764df487e38784aa09
Chris <chris.a.krauter@gmail.com>
2020-07-24 23:36:19 +0200

    Added dataprocessing evalutation

---
+++ src/evaluate/evaluate.go
@@ -0,0 +1,53 @@
+package evaluate
+
+import (
+	"../dataprocessing"
+	//"../server"
+	"encoding/json"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// DataProcessing is evaluated
+func DataProcessing(pbfFileName, note string, xSize, ySize int, createTestGeoJSON, createCoastlineGeoJSON, lessMemory, noBoundingTree bool) {
+	var logging = dataprocessing.Start(pbfFileName, xSize, ySize, createTestGeoJSON, createCoastlineGeoJSON, lessMemory, noBoundingTree)
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	logging["pbfFileName"] = pbfFileName
+	logging["note"] = note
+	logging["xSize"] = strconv.Itoa(xSize)
+	logging["ySize"] = strconv.Itoa(ySize)
+	logging["numCPU"] = strconv.Itoa(runtime.NumCPU())
+	logging["totalAlloc"] = strconv.FormatUint(m.TotalAlloc/1024/1024, 10)
+
+	jsonString, _ := json.Marshal(logging)
+	var filename string
+	var timestamp = time.Now().Format("2006-01-02_15-04-05")
+	if val, ok := logging["filename"]; ok {
+		filename = fmt.Sprintf("data/evaluation/dp_%s_%s_%s%s_%s.json", strings.Split(logging["pbfFileName"], ".")[0], logging["xSize"], logging["ySize"], val, timestamp)
+	} else {
+		filename = fmt.Sprintf("data/evaluation/dp_%s_%s_%s_%s.json", strings.Split(logging["pbfFileName"], ".")[0], logging["xSize"], logging["ySize"], timestamp)
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = f.WriteString(string(jsonString))
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
