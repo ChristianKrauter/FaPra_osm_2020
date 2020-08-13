220006597cb894e2a001eba6daf8d84e4e6920a5
Chris <chris.a.krauter@gmail.com>
2020-08-13 23:58:21 +0200

    Pulled bounding box tree in own file

---
+++ src/dataprocessing/boundingBoxTree.go
@@ -0,0 +1,83 @@
+package dataprocessing
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+type boundingTree struct {
+	boundingBox map[string]float64
+	id          int
+	children    []boundingTree
+}
+
+func createBoundingTree(boundingTreeRoot *boundingTree, allCoastlines *[][][]float64) string {
+	start := time.Now()
+
+	*boundingTreeRoot = boundingTree{map[string]float64{"minX": math.Inf(-1), "maxX": math.Inf(1), "minY": math.Inf(-1), "maxY": math.Inf(1)}, -1, make([]boundingTree, 0)}
+
+	for j, i := range *allCoastlines {
+		bb := createBoundingBox(&i)
+		*boundingTreeRoot = addBoundingTree(boundingTreeRoot, &bb, j)
+	}
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Printf("Created bounding tree            : %s\n", elapsed)
+	return elapsed.String()
+}
+
+func boundingContains(bounding *map[string]float64, point []float64) bool {
+	if (*bounding)["minX"] <= point[0] && point[0] <= (*bounding)["maxX"] {
+		if (*bounding)["minY"] <= point[1] && point[1] <= (*bounding)["maxY"] {
+			return true
+		}
+	}
+	return false
+}
+
+func createBoundingBox(polygon *[][]float64) map[string]float64 {
+	minX := math.Inf(1)
+	maxX := math.Inf(-1)
+	minY := math.Inf(1)
+	maxY := math.Inf(-1)
+	for _, coord := range *polygon {
+		if coord[0] < minX {
+			minX = coord[0]
+		} else if coord[0] > maxX {
+			maxX = coord[0]
+		}
+		if coord[1] < minY {
+			minY = coord[1]
+		} else if coord[1] > maxY {
+			maxY = coord[1]
+		}
+	}
+	return map[string]float64{"minX": minX, "maxX": maxX, "minY": minY, "maxY": maxY}
+}
+
+//Check if a bounding box is inside another bounding box
+func checkBoundingBoxes(bb1 map[string]float64, bb2 map[string]float64) bool {
+	return bb1["minX"] >= bb2["minX"] && bb1["maxX"] <= bb2["maxX"] && bb1["minY"] >= bb2["minY"] && bb1["maxY"] <= bb2["maxY"]
+}
+
+func addBoundingTree(tree *boundingTree, boundingBox *map[string]float64, id int) boundingTree {
+	for i, child := range (*tree).children {
+		if checkBoundingBoxes(*boundingBox, child.boundingBox) {
+			child = addBoundingTree(&child, boundingBox, id)
+			(*tree).children[i] = child
+			return *tree
+		}
+	}
+	(*tree).children = append((*tree).children, boundingTree{*boundingBox, id, make([]boundingTree, 0)})
+	return *tree
+}
+
+func countBoundingTree(bt boundingTree) int {
+	count := 1
+	for _, j := range bt.children {
+		count = count + countBoundingTree(j)
+	}
+	return count
+}
