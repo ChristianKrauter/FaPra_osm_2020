7d4046c72ec85464a4bd808a7db3937ff6e7129a
jonas.a.s.vogelsang <Jonas.a.s.Vogelsang@gmail.com>
2020-08-17 18:01:21 +0200

    deleted unnecessery stuff + lon-lat
    changed everything to lon, lat

--- src/dataprocessing/pipsphere.go
+++ src/dataprocessing/pipsphere.go
@@ -8,14 +8,14 @@ import (
 func transformLon(newNorth, point []float64) float64 {
 	var transformedLon float64
 
-	var dtr = math.Pi / 180
+	var dtr = math.Pi / 180.0
 
 	// New north is already the north pole
-	if newNorth[0] == 90.0 {
+	if newNorth[1] == 90.0 {
 		transformedLon = point[0]
 	} else {
-		var t = math.Sin((point[1]-newNorth[1])*dtr) * math.Cos(point[0]*dtr)
-		var b = math.Sin(dtr*point[0])*math.Cos(newNorth[0]*dtr) - math.Cos(point[0]*dtr)*math.Sin(newNorth[0]*dtr)*math.Cos((point[1]-newNorth[1])*dtr)
+		var t = math.Sin((point[0]-newNorth[0])*dtr) * math.Cos(point[1]*dtr)
+		var b = math.Sin(dtr*point[1])*math.Cos(newNorth[1]*dtr) - math.Cos(point[1]*dtr)*math.Sin(newNorth[1]*dtr)*math.Cos((point[0]-newNorth[0])*dtr)
 		transformedLon = math.Atan2(t, b) / dtr
 	}
 
@@ -50,48 +50,48 @@ func pointInPolygonSphere(polygon *[][]float64, point []float64) bool {
 
 	// Point is the south-pole
 	// Pontentially antipodal check
-	if point[0] == -90 {
+	if point[1] == -90 {
 		fmt.Printf("Tried to check point antipodal to the north pole.")
 		return true
 	}
 
 	// Point is the north-pole
-	if point[0] == 90 {
+	if point[1] == 90 {
 		return false
 	}
 
 	for i := 0; i < len(*polygon); i++ {
 		var a = (*polygon)[i]
-		var b = (*polygon)[i+1%len(*polygon)]
+		var b = (*polygon)[(i+1)%len(*polygon)]
 
-		if point[1] == a[1] {
+		if point[0] == a[0] {
 			strike = true
 		} else {
-			var aToB = eastOrWest(a[1], b[1])
-			var aToP = eastOrWest(a[1], point[1])
-			var pToB = eastOrWest(point[1], b[1])
+			var aToB = eastOrWest(a[0], b[0])
+			var aToP = eastOrWest(a[0], point[0])
+			var pToB = eastOrWest(point[0], b[0])
 			if aToP == aToB && pToB == aToB {
 				strike = true
 			}
 		}
 
 		if strike {
-			if point[0] == a[0] && point[1] == a[1] {
+			if point[1] == a[1] && point[0] == a[0] {
 				return true
 			}
 
 			// Possible to calculate once at polygon creation
-			var northPoleLonTransformed = transformLon(a, []float64{90, 0})
-			var bLonTransformed = transformLon(a, []float64{90, 0})
+			var northPoleLonTransformed = transformLon(a, []float64{0, 90})
+			var bLonTransformed = transformLon(a, b)
 			// Not possible
-			var pLonTransformed = transformLon(a, []float64{90, 0})
+			var pLonTransformed = transformLon(a, point)
 
 			if bLonTransformed == pLonTransformed {
 				return true
 			}
 
-			var bToX = eastOrWest(b[1], northPoleLonTransformed)
-			var bToP = eastOrWest(b[1], point[1])
+			var bToX = eastOrWest(b[0], northPoleLonTransformed)
+			var bToP = eastOrWest(b[0], point[0])
 			if bToX == -bToP {
 				inside = !inside
 			}
@@ -101,42 +101,20 @@ func pointInPolygonSphere(polygon *[][]float64, point []float64) bool {
 	return inside
 }
 
-func rayCastSphere(point, s, e []float64) (bool, bool) {
-	if s[0] > e[0] {
-		s, e = e, s
-	}
-	return true, true
-}
-
-func polygonContainsSphere(polygon *[][]float64, point []float64) bool {
-	b, on := rayCastSphere(point, (*polygon)[0], (*polygon)[len(*polygon)-1])
-	if on {
-		return true
-	}
-
-	for i := 0; i < len(*polygon)-1; i++ {
-		inter, on := rayCastSphere(point, (*polygon)[i], (*polygon)[i+1])
-		if on {
-			return true
-		}
-		if inter {
-			b = !b
-		}
-	}
-	return b
-}
-
 func isLandSphere(tree *boundingTree, point []float64, allCoastlines *[][][]float64) bool {
 	land := false
 	if boundingContains(&tree.boundingBox, point) {
+		if (*tree).id >= 0 {
+			land = pointInPolygonSphere(&(*allCoastlines)[(*tree).id], point)
+			if land {
+				return land
+			}
+		}
 		for _, child := range (*tree).children {
 			land = isLandSphere(&child, point, allCoastlines)
 			if land {
 				return land
 			}
-		}
-		if (*tree).id >= 0 {
-			land = polygonContainsSphere(&(*allCoastlines)[(*tree).id], point)
 		}
 	}
 	return land
@@ -144,8 +122,8 @@ func isLandSphere(tree *boundingTree, point []float64, allCoastlines *[][][]floa
 
 func isLandSphereNBT(allBoundingBoxes *[]map[string]float64, point []float64, allCoastlines *[][][]float64) bool {
 	for i, j := range *allBoundingBoxes {
-		if boundingContains(&j, []float64{point[0], point[1]}) {
-			if polygonContainsSphere(&(*allCoastlines)[i], []float64{point[0], point[1]}) {
+		if boundingContains(&j, point) {
+			if pointInPolygonSphere(&(*allCoastlines)[i], point) {
 				return true
 			}
 		}
