d4dc4c62c8c080f18b3932efd2dcda6c0d4596d0
Christian Krauter <chris.a.krauter@gmail.com>
2020-08-24 16:18:54 +0200

    Updated meshgrid bash

--- makegrids.bash
+++ makegrids.bash
@@ -4,22 +4,18 @@ echo $(date)
 echo "--------------------"
 
 printf "ug 100x500"
-go run osmgw.go -m=2 -x=100 -y=500
-printf "ug 360x360"
-go run osmgw.go -m=2 -x=360 -y=360
+go run osmgw.go -m=2 -x=100 -y=500 -f=planet-coastlines.pbf
 printf "ug 1000x500"
-go run osmgw.go -m=2 -x=1000 -y=500
+go run osmgw.go -m=2 -x=1000 -y=500 -f=planet-coastlines.pbf
 printf "ug 1000x1000"
-go run osmgw.go -m=2 -x=1000 -y=1000
+go run osmgw.go -m=2 -x=1000 -y=1000 -f=planet-coastlines.pbf
 
-printf "bg 100x500"
-go run osmgw.go -m=2 -bg -x=100 -y=500
 printf "bg 360x360"
-go run osmgw.go -m=2 -bg -x=360 -y=360
+go run osmgw.go -m=2 -bg -x=360 -y=360 -f=planet-coastlines.pbf
 printf "bg 1000x500"
-go run osmgw.go -m=2 -bg -x=1000 -y=500
+go run osmgw.go -m=2 -bg -x=1000 -y=500 -f=planet-coastlines.pbf
 printf "bg 1000x1000"
-go run osmgw.go -m=2 -bg -x=1000 -y=1000
+go run osmgw.go -m=2 -bg -x=1000 -y=1000 -f=planet-coastlines.pbf
 
 
 echo "--------------------"
