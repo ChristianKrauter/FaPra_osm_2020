546f4fd5fa4ea22d694194f514902e7a8d625fd5
Christian Krauter <chris.a.krauter@gmail.com>
2020-08-27 16:31:31 +0200

    Reduced code for Bidirectional Dijkstra
    Acces dist, pq and proc structures with index

--- src/algorithms/bidijkstra.go
+++ src/algorithms/bidijkstra.go
@@ -3,235 +3,73 @@ package algorithms
 import (
 	"../grids"
 	"container/heap"
-	"fmt"
 	"math"
 )
 
 // BiDijkstra implementation on uniform grid
 func BiDijkstra(fromIDX, toIDX []int, ug *grids.UniformGrid) ([][][]float64, int) {
 
-	var prev [][]int
-	var dist []float64
-	var distR []float64
-	pq := make(priorityQueue, 1)
-	pqR := make(priorityQueue, 1)
-	proc := make(map[int]bool)
-	procR := make(map[int]bool)
-
-	var meeting int
-	var bestDist = math.Inf(1)
-	var minF = math.Inf(1)
-	var minR = math.Inf(1)
-
-	for i := 0; i < (*ug).N; i++ {
-		dist = append(dist, math.Inf(1))
-		distR = append(dist, math.Inf(1))
-		prev = append(prev, []int{-1, -1})
-	}
-
-	dist[(*ug).GridToID(fromIDX)] = 0
-	pq[0] = &Item{
-		value:    (*ug).GridToID(fromIDX),
-		priority: 0,
-		index:    0,
-	}
-	heap.Init(&pq)
-
-	// Reverse
-	distR[(*ug).GridToID(toIDX)] = 0
-	pqR[0] = &Item{
-		value:    (*ug).GridToID(toIDX),
-		priority: 0,
-		index:    0,
-	}
-	heap.Init(&pqR)
-
-	for {
-		if len(pq) == 0 {
-			break
-		} else {
-			u := heap.Pop(&pq).(*Item).value
-
-			if procR[u] {
-				fmt.Printf("bestDist         %v\n", bestDist)
-				fmt.Printf("dist[u]+distR[u] %v\n", dist[u]+distR[u])
-				bestDist = math.Min(bestDist, dist[u]+distR[u])
-				//var minF = math.MaxFloat64
-				//var minR = math.MaxFloat64
-				for _, k := range pqR {
-					if proc[k.value] {
-						//fmt.Printf("[k]: %v\n", k.value)
-						if distR[k.value] < minR {
-							//fmt.Printf("distR < minR: %v < %v\n", distR[k.value], minR)
-							minR = distR[k.value]
-						} /*else  {
-							fmt.Printf("distR >= minR: %v >= %v\n", distR[k.value], minR)
-						}*/
-					}
-				}
-
-				for _, k := range pq {
-					if procR[k.value] {
-						//fmt.Printf("[k]: %v\n", k.value)
-						if dist[k.value] < minF {
-							//fmt.Printf("dist < min: %v < %v\n", distR[k.value], minR)
-							minF = dist[k.value]
-						} /*else {
-							fmt.Printf("dist >= minF: %v >= %v\n", dist[k.value], minF)
-						} */
-					}
-				}
-				fmt.Printf("minR+minF - bestDist: %v - %v\n", minR+minF, bestDist)
-				if minR+minF >= bestDist {
-					//meeting = u
-					break
-				}
-			}
-
-			neighbours := NeighboursUg(u, ug)
-			for _, j := range neighbours {
-				var alt = dist[u] + distance((*ug).GridToCoord((*ug).IDToGrid(u)), (*ug).GridToCoord((*ug).IDToGrid(j)))
-				if alt < dist[j] {
-					dist[j] = alt
-					/*if dist[j] < minF {
-						minF = dist[j]
-					}*/
-					prev[j][0] = u
-					item := &Item{
-						value:    j,
-						priority: -dist[j],
-					}
-					heap.Push(&pq, item)
-				}
-			}
-			proc[u] = true
-
-		}
-
-		// Reverse
-		if len(pqR) == 0 {
-			break
-		} else {
-			u := heap.Pop(&pqR).(*Item).value
-
-			if proc[u] {
-				fmt.Printf("bestDist    %v\n", bestDist)
-				bestDist = math.Min(bestDist, dist[u]+distR[u])
-				fmt.Printf("bestDistNEW %v\n", bestDist)
-				//var minF = math.MaxFloat64
-				//var minR = math.MaxFloat64
-				for _, k := range pqR {
-					if proc[k.value] {
-						//fmt.Printf("[k]: %v\n", k.value)
-						if distR[k.value] < minR {
-							//fmt.Printf("distR < minR: %v < %v\n", distR[k.value], minR)
-							minR = distR[k.value]
-						} /*else  {
-							fmt.Printf("distR >= minR: %v >= %v\n", distR[k.value], minR)
-						}*/
-					}
-				}
-
-				for _, k := range pq {
-					if procR[k.value] {
-						//fmt.Printf("[k]: %v\n", k.value)
-						if dist[k.value] < minF {
-							//fmt.Printf("dist < min: %v < %v\n", distR[k.value], minR)
-							minF = dist[k.value]
-						} /*else {
-							fmt.Printf("dist >= minF: %v >= %v\n", dist[k.value], minF)
-						} */
-					}
-				}
-
-				fmt.Printf("minR+minF - bestDist: %v - %v\n", minR+minF, bestDist)
-				if minR+minF >= bestDist {
-					meeting = u
-					break
-				}
-			}
-
-			neighbours := NeighboursUg(u, ug)
-			for _, j := range neighbours {
-				var alt = distR[u] + distance((*ug).GridToCoord((*ug).IDToGrid(u)), (*ug).GridToCoord((*ug).IDToGrid(j)))
-				if alt < distR[j] {
-					distR[j] = alt
-					prev[j][1] = u
-					item := &Item{
-						value:    j,
-						priority: -distR[j],
-					}
-					heap.Push(&pqR, item)
-				}
-			}
-			procR[u] = true
-		}
-
-	}
-	var route = ExtractRouteUgBi(&prev, meeting, ug)
-	return route, len(procR) + len(proc)
+	return make([][][]float64, 0), 0
 }
 
 // BiDijkstraAllNodes additionally returns all visited nodes on uniform grid
 func BiDijkstraAllNodes(fromIDX, toIDX []int, ug *grids.UniformGrid) ([][][]float64, [][]float64) {
 
 	var prev [][]int
-	var dist []float64
-	var distR []float64
-	pq := make(priorityQueue, 1)
-	pqR := make(priorityQueue, 1)
-	proc := make(map[int]bool)
-	procR := make(map[int]bool)
-
+	dist := make([][]float64, 2)
+	pq := []priorityQueue{make(priorityQueue, 1), make(priorityQueue, 1)}
+	proc := []map[int]bool{make(map[int]bool), make(map[int]bool)}
 	var meeting int
 
+	// Init
 	for i := 0; i < (*ug).N; i++ {
-		dist = append(dist, math.Inf(1))
-		distR = append(distR, math.Inf(1))
+		dist[0] = append(dist[0], math.Inf(1))
+		dist[1] = append(dist[1], math.Inf(1))
 		prev = append(prev, []int{-1, -1})
 	}
 
-	dist[(*ug).GridToID(fromIDX)] = 0
-	pq[0] = &Item{
+	dist[0][(*ug).GridToID(fromIDX)] = 0
+	pq[0][0] = &Item{
 		value:    (*ug).GridToID(fromIDX),
 		priority: 0,
 		index:    0,
 	}
-	heap.Init(&pq)
+	heap.Init(&pq[0])
 
-	// Reverse
-	distR[(*ug).GridToID(toIDX)] = 0
-	pqR[0] = &Item{
+	dist[1][(*ug).GridToID(toIDX)] = 0
+	pq[1][0] = &Item{
 		value:    (*ug).GridToID(toIDX),
 		priority: 0,
 		index:    0,
 	}
-	heap.Init(&pqR)
+	heap.Init(&pq[1])
 
+	var dir = 0 // Direction in which next step should be taken. 0=forward, 1=backward
+	// Main loop
 	for {
-		if len(pq) == 0 {
+		if len(pq[0]) == 0 || len(pq[1]) == 0 {
 			break
 		} else {
-			u := heap.Pop(&pq).(*Item).value
-			proc[u] = true
+			u := heap.Pop(&pq[dir]).(*Item).value
+			proc[dir][u] = true
 
-			if procR[u] {
-				var bestDist = dist[u] + distR[u]
+			if proc[1-dir][u] {
+				var bestDist = dist[0][u] + dist[1][u]
 				meeting = u
 
-				for _, k := range pqR {
-					if proc[k.value] {
-						if distR[k.value]+dist[k.value] < bestDist {
-							bestDist = distR[k.value] + dist[k.value]
+				for _, k := range pq[0] {
+					if proc[1][k.value] {
+						if dist[0][k.value]+dist[1][k.value] < bestDist {
+							bestDist = dist[0][k.value] + dist[1][k.value]
 							meeting = k.value
 						}
 					}
 				}
 
-				for _, k := range pq {
-					if procR[k.value] {
-						if dist[k.value]+distR[k.value] < bestDist {
-							bestDist = distR[k.value] + dist[k.value]
+				for _, k := range pq[1] {
+					if proc[0][k.value] {
+						if dist[0][k.value]+dist[1][k.value] < bestDist {
+							bestDist = dist[0][k.value] + dist[1][k.value]
 							meeting = k.value
 						}
 					}
@@ -241,73 +79,28 @@ func BiDijkstraAllNodes(fromIDX, toIDX []int, ug *grids.UniformGrid) ([][][]floa
 
 			neighbours := NeighboursUg(u, ug)
 			for _, j := range neighbours {
-				var alt = dist[u] + distance((*ug).GridToCoord((*ug).IDToGrid(u)), (*ug).GridToCoord((*ug).IDToGrid(j)))
-				if alt < dist[j] {
-					dist[j] = alt
-					prev[j][0] = u
+				var alt = dist[dir][u] + distance((*ug).GridToCoord((*ug).IDToGrid(u)), (*ug).GridToCoord((*ug).IDToGrid(j)))
+				if alt < dist[dir][j] {
+					dist[dir][j] = alt
+					prev[j][dir] = u
 					item := &Item{
 						value:    j,
-						priority: -dist[j],
+						priority: -dist[dir][j],
 					}
-					heap.Push(&pq, item)
+					heap.Push(&pq[dir], item)
 				}
 			}
 		}
-
-		// Reverse
-		if len(pqR) == 0 {
-			break
-		} else {
-			u := heap.Pop(&pqR).(*Item).value
-			procR[u] = true
-
-			if proc[u] {
-				var bestDist = dist[u] + distR[u]
-				meeting = u
-
-				for _, k := range pq {
-					if procR[k.value] {
-						if dist[k.value]+distR[k.value] < bestDist {
-							bestDist = dist[k.value] + distR[k.value]
-							meeting = k.value
-						}
-					}
-				}
-				for _, k := range pqR {
-					if proc[k.value] {
-						if dist[k.value]+distR[k.value] < bestDist {
-							bestDist = dist[k.value] + distR[k.value]
-							meeting = k.value
-						}
-					}
-				}
-
-				break
-			}
-
-			neighbours := NeighboursUg(u, ug)
-			for _, j := range neighbours {
-				var alt = distR[u] + distance((*ug).GridToCoord((*ug).IDToGrid(u)), (*ug).GridToCoord((*ug).IDToGrid(j)))
-				if alt < distR[j] {
-					distR[j] = alt
-					prev[j][1] = u
-					item := &Item{
-						value:    j,
-						priority: -distR[j],
-					}
-					heap.Push(&pqR, item)
-				}
-			}
-		}
+		dir = 1 - dir // Change direction
 	}
 
-	keys := make([]int, len(proc)+len(procR))
+	keys := make([]int, len(proc[0])+len(proc[1]))
 	i := 0
-	for k := range proc {
+	for k := range proc[0] {
 		keys[i] = k
 		i++
 	}
-	for k := range procR {
+	for k := range proc[1] {
 		keys[i] = k
 		i++
 	}
