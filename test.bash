26067b496ad6d15213cd67bd617203e7a049325c
Chris <chris.a.krauter@gmail.com>
2020-09-02 21:18:56 +0200

    Improved Test bash file

--- test.bash
+++ test.bash
@@ -1,82 +1,95 @@
 ## Test all different options
-echo "Tests on:" >> test.txt
-echo $(date) >> test.txt
-echo "--------------------" >> test.txt
-echo "--------------------" >> test.txt
+printf "Starting tests:"
+printf "\n===========================================================\n"
 
-printf "Server tests (you have to terminate each server manually):\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Server tests:" >> test.txt
-go run osmgw.go >> test.txt
-go run osmgw.go -bpip >> test.txt
-go run osmgw.go -bg >> test.txt
-go run osmgw.go -bg -bpip >> test.txt
+printf "\n\nServer tests (you have to terminate each server manually):"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go
+printf "\n"
+go run osmgw.go -bpip
+printf "\n"
+go run osmgw.go -bg
+printf "\n"
+go run osmgw.go -bg -bpip
+printf "\n"
 
-printf "Dataprocessing tests:\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Dataprocessing tests:" >> test.txt
-go run osmgw.go -m=1 >> test.txt
-go run osmgw.go -m=1 -lm >> test.txt
-go run osmgw.go -m=1 -coastline >> test.txt
-go run osmgw.go -m=1 -bpip >> test.txt
-go run osmgw.go -m=1 -nbt >> test.txt
-go run osmgw.go -m=1 -nbt -bpip >> test.txt
+printf "\nDataprocessing tests:"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go -m=1
+printf "\n\n"
+go run osmgw.go -m=1 -lm
+printf "\n\n"
+go run osmgw.go -m=1 -coastline
+printf "\n\n"
+go run osmgw.go -m=1 -bpip
+printf "\n\n"
+go run osmgw.go -m=1 -nbt
+printf "\n\n"
+go run osmgw.go -m=1 -nbt -bpip
+printf "\n\n"
 
-printf "Basic grid dataprocessing tests:\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Basic grid dataprocessing tests:" >> test.txt
-go run osmgw.go -m=1 -bg >> test.txt
-go run osmgw.go -m=1 -bg -coastline >> test.txt
-go run osmgw.go -m=1 -bg -bpip >> test.txt
-go run osmgw.go -m=1 -bg -nbt >> test.txt
-go run osmgw.go -m=1 -bg -nbt -bpip >> test.txt
+printf "\nBasic grid dataprocessing tests:"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go -m=1 -bg
+printf "\n\n"
+go run osmgw.go -m=1 -bg -coastline
+printf "\n\n"
+go run osmgw.go -m=1 -bg -bpip
+printf "\n\n"
+go run osmgw.go -m=1 -bg -nbt
+printf "\n\n"
+go run osmgw.go -m=1 -bg -nbt -bpip
+printf "\n\n"
 
-printf "Eval dataprocessing tests:\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Eval dataprocessing tests:" >> test.txt
-go run osmgw.go -m=2 >> test.txt
-go run osmgw.go -m=2 -lm >> test.txt
-go run osmgw.go -m=2 -coastline >> test.txt
-go run osmgw.go -m=2 -bpip >> test.txt
-go run osmgw.go -m=2 -nbt >> test.txt
-go run osmgw.go -m=2 -nbt -bpip >> test.txt
+printf "\nEval dataprocessing tests:"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go -m=2
+printf "\n\n"
+go run osmgw.go -m=2 -lm
+printf "\n\n"
+go run osmgw.go -m=2 -coastline
+printf "\n\n"
+go run osmgw.go -m=2 -bpip
+printf "\n\n"
+go run osmgw.go -m=2 -nbt
+printf "\n\n"
+go run osmgw.go -m=2 -nbt -bpip
+printf "\n\n"
 
-printf "Eval basic grid dataprocessing tests:\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Eval basic grid dataprocessing tests:" >> test.txt
-go run osmgw.go -m=2 -bg >> test.txt
-go run osmgw.go -m=2 -bg -coastline >> test.txt
-go run osmgw.go -m=2 -bg -bpip >> test.txt
-go run osmgw.go -m=2 -bg -nbt >> test.txt
-go run osmgw.go -m=2 -bg -nbt -bpip >> test.txt
+printf "\nEval basic grid dataprocessing tests:"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go -m=2 -bg
+printf "\n\n"
+go run osmgw.go -m=2 -bg -coastline
+printf "\n\n"
+go run osmgw.go -m=2 -bg -bpip
+printf "\n\n"
+go run osmgw.go -m=2 -bg -nbt
+printf "\n\n"
+go run osmgw.go -m=2 -bg -nbt -bpip
+printf "\n\n"
 
-printf "Eval wayfinding tests:\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Eval wayfinding tests:" >> test.txt
-go run osmgw.go -m=3 -r=100 -bg >> test.txt
-echo "" >> test.txt
-go run osmgw.go -m=3 -r=100 -bg -bpip >> test.txt
-echo "" >> test.txt
-go run osmgw.go -m=3 -r=100 >> test.txt
-echo "" >> test.txt
-go run osmgw.go -m=3 -r=100 -bpip >> test.txt
-echo "" >> test.txt
+printf "\nEval wayfinding tests:"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go -m=3 -r=10 -bg
+printf "\n\n"
+go run osmgw.go -m=3 -r=10 -bg -bpip
+printf "\n\n"
+go run osmgw.go -m=3 -r=10
+printf "\n\n"
+go run osmgw.go -m=3 -r=10 -bpip
+printf "\n\n"
+go run osmgw.go -m=3 -a=1 -r=10
+printf "\n\n"
+go run osmgw.go -m=3 -a=2 -r=10
+printf "\n\n"
+go run osmgw.go -m=3 -a=3 -r=10
+printf "\n\n"
 
-printf "Eval readpbf test:\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "Eval readpbf test:" >> test.txt
-go run osmgw.go -m=4 >> test.txt
+printf "Eval readpbf test:"
+printf "\n-----------------------------------------------------------\n"
+go run osmgw.go -m=4
+printf "\n"
 
-printf "Testing done, see test.txt for the results.\n"
-echo "" >> test.txt
-echo "--------------------" >> test.txt
-echo "--------------------" >> test.txt
-echo "Testing done, see test.txt for the results." >> test.txt
-echo "" >> test.txt
+printf "\n===========================================================\n"
+printf "Testing done."
